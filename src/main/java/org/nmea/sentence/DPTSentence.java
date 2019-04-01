de94210895441aa30f300fa717879c36704d02bf
Jesse Caulfield <jesse.caulfield@keybridgewireless.com>
2019-04-01 08:15:02 -0400

    set gpl v3 license

--- src/main/java/org/nmea/sentence/DPTSentence.java
+++ src/main/java/org/nmea/sentence/DPTSentence.java
@@ -1,22 +1,18 @@
-/*
- * DPTSentence.java
- * Copyright (C) 2011 Kimmo Tuukkanen
+/* 
+ * Copyright (C) 2019 Key Bridge
  *
- * This file is part of Java Marine API.
- * <http://ktuukkan.github.io/marine-api/>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Java Marine API is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or (at your
- * option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * Java Marine API is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
- * for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Java Marine API. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.nmea.sentence;
 
